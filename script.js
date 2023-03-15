e1111cc4832e159957e218ba9aacb6523a7494d3
Yash Goyal <yashgoyal1620@gmail.com>
2023-03-15 09:19:03 +0530

    updated

--- script.js
+++ script.js
@@ -37,3 +37,30 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 }; 
+
+// scroll revel
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form' ,{ origin: 
+'bottom' });
+ScrollReveal().reveal('.home-content h1 , .about-img' ,{ origin: 
+'left' });
+ScrollReveal().reveal('.home-content p, .about-content' ,{ origin: 
+    'right' });
+
+
+    // typed JS
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer' , 'Flutter Developer', 'Java Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
